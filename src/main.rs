b78c5017937b4a560428f2a049a440aa3ff92ee1
Lukas Ziegler <lziegler@oss-group.co.nz>
2021-12-27 17:07:51 +1300

    counting materials now

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,46 @@
-use std::collections::HashMap;
-use std::error::Error;
+use std::collections::BTreeMap;
 use std::fs::File;
 
 use clap::{App, Arg};
-use fastanvil::RegionBuffer;
-use fastnbt::Value;
+use fastanvil::{Chunk, JavaChunk, RegionBuffer};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let matches = App::new("mc-block-stats")
         .arg(Arg::with_name("file").required(true))
         .get_matches();
 
     let file = matches.value_of("file").expect("file is required");
     let file = File::open(file).expect("file does not exist");
+    let mut final_counts: BTreeMap<String, isize> = BTreeMap::new();
 
     let mut region = RegionBuffer::new(file);
 
-    region
-        .for_each_chunk(|x, z, data| {
-            println!("Processing Chunk: x={} / z={}", x, z);
-
-            let compound: HashMap<String, Value> =
-                fastnbt::de::from_bytes(data.as_slice()).unwrap();
-            match compound["DataVersion"] {
-                Value::Int(ver) => println!("Version: {}", ver),
-                _ => {}
+    let _ = region.for_each_chunk(|x, z, data| {
+        let chunk: JavaChunk = fastnbt::de::from_bytes(data.as_slice()).unwrap();
+        println!(
+            "Chunk( x={}, z={} ) -> y = [{}..{}]",
+            x,
+            z,
+            chunk.y_range().start,
+            chunk.y_range().end
+        );
+        let mut counts: BTreeMap<&str, isize> = BTreeMap::new();
+        for chunk_y in chunk.y_range() {
+            for chunk_x in 0..15 {
+                for chunk_z in 0..15 {
+                    let material = chunk.block(chunk_x, chunk_y, chunk_z).unwrap().name();
+                    if material != "minecraft:air" {
+                        *(counts.entry(material).or_insert(0)) += 1;
+                    }
+                }
             }
-            // println!("{:#?}", compound);
-        })
-        .map_err(|e| e.into())
+        }
+        for (material, count) in counts {
+            *(final_counts.entry(material.to_string()).or_insert(0)) += count;
+        }
+    });
+
+    for (k, v) in final_counts {
+        println!("{} -> {}", k, v);
+    }
 }
