b0c3ad040cfd7a3a4039ec68a73bd4c11de0bee0
Lukas Ziegler <lziegler@oss-group.co.nz>
2021-12-28 18:57:49 +1300

    restructured code, added logging

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,78 @@
-use std::collections::BTreeMap;
-use std::fs::File;
-use std::sync::mpsc::channel;
-
-use clap::{App, Arg};
-use threadpool::ThreadPool;
-
 use fastanvil::{Chunk, JavaChunk, RegionBuffer};
+use log::*;
+use std::{collections::BTreeMap, fs::File, sync::mpsc::channel};
+use structopt::StructOpt;
+use threadpool::ThreadPool;
 
 const IGNORE_BLOCKS: &[&str] = &["minecraft:air", "minecraft:cave_air"];
 const CHUNK_FULL: &str = "full";
 
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    /// Silence all output
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+    /// Verbose mode (-v, -vv, -vvv, etc)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: usize,
+    /// Minecraft region file (*.mca)
+    #[structopt(short = "f", long = "region-file")]
+    region_file: String,
+    /// Number of concurrent threads (defaults to number of CPU cores)
+    #[structopt(short = "t", long = "threads")]
+    threads: Option<usize>,
+}
+
 fn main() {
-    let matches = App::new("mc-block-stats")
-        .arg(Arg::with_name("file").required(true))
-        .get_matches();
+    // parse CLI arguments
+    let opt = Opt::from_args();
 
-    //    let file_path = matches.value_of("file").expect("file is required");
+    // initialise logger
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(opt.quiet)
+        .verbosity(opt.verbose + 1)
+        .timestamp(stderrlog::Timestamp::Millisecond)
+        .init()
+        .unwrap();
 
-    let pool = ThreadPool::new(num_cpus::get());
+    // initialise thread pool
+    let threads = opt.threads.unwrap_or_default();
+    let pool_size = if threads == 0 {
+        num_cpus::get()
+    } else {
+        threads
+    };
+    info!("Using up to {} threads.", pool_size);
+    let pool = ThreadPool::new(pool_size);
+
     let (tx, rx) = channel();
+
+    // process Minecraft region files
     {
         // TODO: loop over region files
         let tx = tx.clone();
 
+        // process each region file in separate thread
         pool.execute(move || {
-            let file_path = matches.value_of("file").expect("file is required");
+            let file_path = opt.region_file;
+
+            info!("Processing file {}...", file_path);
             let file = File::open(file_path).expect("file does not exist");
 
-            let mut region = RegionBuffer::new(file);
-            let mut region_counts: BTreeMap<String, Vec<isize>> = BTreeMap::new();
-
-            let _ = region.for_each_chunk(|x, z, data| {
-                let chunk: JavaChunk = fastnbt::de::from_bytes(data.as_slice()).unwrap();
-                println!(
-                    "Chunk( x={}, z={}, status={} ) -> y = [{}..{}]",
-                    x,
-                    z,
-                    chunk.status(),
-                    chunk.y_range().start,
-                    chunk.y_range().end
-                );
-
-                if chunk.status() == CHUNK_FULL {
-                    // let world_height = usize::try_from(chunk.y_range().end - chunk.y_range().start).unwrap();
-                    let world_height = 256; // hard-coded for 1.17 -> will break things for 1.18!
-                    let height_offset = 0 - chunk.y_range().start;
-
-                    for chunk_y in chunk.y_range() {
-                        let counter_idx = usize::try_from(height_offset + chunk_y).unwrap();
-                        for chunk_x in 0..16 {
-                            for chunk_z in 0..16 {
-                                let block_type =
-                                    chunk.block(chunk_x, chunk_y, chunk_z).unwrap().name();
-                                // skip blocks we're not interested in
-                                if !IGNORE_BLOCKS.iter().any(|&i| i == block_type) {
-                                    // can't use the entry API without changing ownership, which is expensive
-                                    if region_counts.contains_key(block_type) {
-                                        (*region_counts.get_mut(block_type).unwrap())
-                                            [counter_idx] += 1;
-                                    } else {
-                                        // need to own block_type name
-                                        region_counts
-                                            .insert(block_type.to_string(), vec![0; world_height]);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            });
+            let region_counts = gather_region_stats(file);
 
             tx.send(region_counts).expect("Could not send data!");
         });
     }
     drop(tx);
 
-    // ### REDUCE phase
+    // process results coming in from each region file
     let mut final_counts: BTreeMap<String, Vec<isize>> = BTreeMap::new();
     for region_counts in rx.iter() {
-        println!(
-            "got result for region [{} threads active]",
+        info!(
+            "Got result for region [{} threads active]",
             pool.active_count()
         );
 
@@ -99,4 +92,50 @@ fn main() {
     for (k, v) in final_counts {
         println!("{} -> {:?}", k, v);
     }
+}
+
+fn gather_region_stats(file: File) -> BTreeMap<String, Vec<isize>> {
+    let mut region = RegionBuffer::new(file);
+    let mut region_counts: BTreeMap<String, Vec<isize>> = BTreeMap::new();
+
+    // process all chunks in region file sequentially
+    let _ = region.for_each_chunk(|x, z, data| {
+        let chunk: JavaChunk = fastnbt::de::from_bytes(data.as_slice()).unwrap();
+
+        debug!(
+            "Processing Chunk( x={}, z={}, status={} ) -> y = [{}..{}]",
+            x,
+            z,
+            &chunk.status(),
+            chunk.y_range().start,
+            chunk.y_range().end
+        );
+
+        // skip incomplete chunks
+        if chunk.status() == CHUNK_FULL {
+            // let world_height = usize::try_from(chunk.y_range().end - chunk.y_range().start).unwrap();
+            let world_height = 256; // hard-coded for 1.17 -> will break things for 1.18!
+            let height_offset = 0 - chunk.y_range().start;
+
+            for chunk_y in chunk.y_range() {
+                let counter_idx = usize::try_from(height_offset + chunk_y).unwrap();
+                for chunk_x in 0..16 {
+                    for chunk_z in 0..16 {
+                        let block_type = chunk.block(chunk_x, chunk_y, chunk_z).unwrap().name();
+                        // skip blocks we're not interested in
+                        if !IGNORE_BLOCKS.iter().any(|&i| i == block_type) {
+                            // can't use the entry API without changing ownership, which is expensive
+                            if region_counts.contains_key(block_type) {
+                                (*region_counts.get_mut(block_type).unwrap())[counter_idx] += 1;
+                            } else {
+                                // need to own block_type name
+                                region_counts.insert(block_type.to_string(), vec![0; world_height]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+    region_counts
 }
